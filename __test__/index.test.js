19895017a1782249c9efe6d3afe2398a91c826c5
MatthieuLemoine <MatthieuLemoine@users.noreply.github.com>
2018-10-10 22:34:41 +0200

    :construction_worker: Setup CI

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,39 +1,39 @@
-const path = require('path')
-const recursive = require('../lib/utils').searchFiles
+const path = require('path');
+const { searchFiles: recursive } = require('../lib/utils');
 
-const dirToSearch = path.resolve(__dirname, 'testdir')
+const dirToSearch = path.resolve(__dirname, 'testdir');
 
 describe('dirToSearch()', () => {
   it('list all directories', async () => {
-    const dirs = await recursive(dirToSearch)
+    const dirs = await recursive(dirToSearch);
     const expected = [
       path.resolve(__dirname, 'testdir/onetest.txt'),
       path.resolve(__dirname, 'testdir/a/test2.txt'),
       path.resolve(__dirname, 'testdir/a/test3.txt'),
       path.resolve(__dirname, 'testdir/b/test4.txt'),
       path.resolve(__dirname, 'testdir/b/test5.txt'),
-    ]
-    expect(dirs.sort()).toEqual(expected.sort())
-  })
+    ];
+    expect(dirs.sort()).toEqual(expected.sort());
+  });
   it('ignores specified files', async () => {
-    const filesToIgnore = 'onetest*'
-    const dirs = await recursive(dirToSearch, [filesToIgnore])
+    const filesToIgnore = 'onetest*';
+    const dirs = await recursive(dirToSearch, [filesToIgnore]);
     const expected = [
       path.resolve(__dirname, 'testdir/a/test2.txt'),
       path.resolve(__dirname, 'testdir/a/test3.txt'),
       path.resolve(__dirname, 'testdir/b/test4.txt'),
       path.resolve(__dirname, 'testdir/b/test5.txt'),
-    ]
-    expect(dirs.sort()).toEqual(expected.sort())
-  })
+    ];
+    expect(dirs.sort()).toEqual(expected.sort());
+  });
   it('ignores all files in specified directories', async () => {
-    const dirsToIgnore = '**/a'
-    const dirs = await recursive(dirToSearch, [dirsToIgnore])
+    const dirsToIgnore = '**/a';
+    const dirs = await recursive(dirToSearch, [dirsToIgnore]);
     const expected = [
       path.resolve(__dirname, 'testdir/onetest.txt'),
       path.resolve(__dirname, 'testdir/b/test4.txt'),
       path.resolve(__dirname, 'testdir/b/test5.txt'),
-    ]
-    expect(dirs.sort()).toEqual(expected.sort())
-  })
-})
+    ];
+    expect(dirs.sort()).toEqual(expected.sort());
+  });
+});
